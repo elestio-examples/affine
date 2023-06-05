286d81debe0bc97bf691e51ef050326a642add3f
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-05 19:45:02 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -2,28 +2,15 @@ pwd=$(pwd)
 
 mv "$pwd"/.github/deployment/Dockerfile "$pwd"
 
-cat << EOT > Dockerfile
-FROM node:16-alpine as relocate
-RUN yarn install
-RUN yarn build
-RUN cd apps/web && yarn install && npm run build && npm run export
-RUN cd ..
-RUN cd ..
-WORKDIR /app
-COPY ./apps/web/out ./dist
-COPY ./.github/deployment/Caddyfile ./Caddyfile
+cd "$pwd"/apps/web
 
+yarn install;
+npm run build;
+npm run export;
 
-FROM caddy:2.6.2-alpine
-ARG API_SERVER
-WORKDIR /app
-COPY --from=relocate /app .
+cd "$pwd"
 
-EXPOSE 3000
-ENV API_SERVER=$API_SERVER
-CMD ["caddy", "run"]
-EOT
-
+sed -i "s~EXPOSE 80~EXPOSE 3000~g" ./Dockerfile
 sed -i "s~:80~:3000~g" "$pwd"/.github/deployment/Caddyfile
 
 docker build . --tag elestio4test/affine:latest;
