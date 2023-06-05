e41ae57a6f93535b51393e2ffa7108ee1e3334fd
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-05 17:16:17 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -2,5 +2,7 @@ pwd=$(pwd)
 
 mv "$pwd"/.github/deployment/Dockerfile "$pwd"
 
+sed -i "s~COPY ./apps/web/out ./dist~COPY ./apps/web ./dist~g" ./Dockerfile
+
 docker build . --tag elestio4test/affine:latest;
 TAG_TO_CHANGE
