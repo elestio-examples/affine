dc25d56af7c41c5553a6983fd6e28b90b36c5285
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-06-08 14:03:00 +0300

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -15,8 +15,6 @@ npm run export;
 
 cd "$pwd"
 
-sed -i "s~COPY ./apps/web/out ./dist~COPY $pwd/apps/web/out ./dist~g" ./Dockerfile
-sed -i "s~COPY ./.github/deployment/Caddyfile ./Caddyfile~COPY $pwd/.github/deployment/Caddyfile ./Caddyfile~g" ./Dockerfile
 sed -i "s~EXPOSE 80~EXPOSE 3000~g" ./Dockerfile
 sed -i "s~:80~:3000~g" "$pwd"/.github/deployment/Caddyfile
 
